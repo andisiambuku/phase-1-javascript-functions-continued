aa6e1de8651e5d5aaad9936d9fd71c087837deca
andisiambuku <roselandambuku@gmail.com>
2022-08-19 20:40:27 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(play="roller-skate"){
+    return `This Saturday, I want to ${play}!`;
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(why="go to the office"){
+    return `This Monday, I will ${why}.`
+}
+mondayWork("work from home");
+
+// function wrapAdjective(flair="*"){
+//     return function(){
+//         const message = "special";
+//     }
+// }
+function wrapAdjective(flair="*") {
+    return function (praise = "special") {
+        return `You are ${flair}${praise}${flair}!`; // 4
+      }; 
+    }
+wrapAdjective("||")("a dedicated programmer");
+
